5066864d002222ecc440d51ee6947f80d86dd771
Elena <e.mandrovska@gmail.com>
2021-05-12 20:42:27 +0300

    task 3 done

--- src/js/makeTransaction.js
+++ src/js/makeTransaction.js
@@ -1,51 +1,59 @@
-// const randomIntegerFromInterval = (min, max) => {
-//   return Math.floor(Math.random() * (max - min + 1) + min);
-// };
+// Перепиши функцию makeTransaction() так, чтобы она не использовала
+// callback - функции onSuccess и onError, а принимала всего один параметр
+// transaction и возвращала промис.
+const taskMakeTransaction = document.querySelector('.task-3');
 
-// const makeTransaction = (transaction, onSuccess, onError) => {
-//   const delay = randomIntegerFromInterval(200, 500);
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
 
-//   setTimeout(() => {
-//     const canProcess = Math.random() > 0.3;
+const makeTransaction = (transaction) => {
+    return new Promise((resolve, reject) => {
+            const delay = randomIntegerFromInterval(200, 500);
+        setTimeout(() => {
+            const canProcess = Math.random() > 0.3;
+            if (canProcess) {
+                resolve({ id: transaction.id, time: delay });
+            } else {
+                reject(transaction.id);
+            }
+        }, delay);
+    });
+}
+const logSuccess = ({id, time}) => {
+  console.log(`Transaction ${id} processed in ${time}ms`);
+};
 
-//     if (canProcess) {
-//       onSuccess(transaction.id, delay);
-//     } else {
-//       onError(transaction.id);
-//     }
-//   }, delay);
-// };
+const logError = id => {
+  console.warn(`Error processing transaction ${id}. Please try again later.`);
+};
 
-// const logSuccess = (id, time) => {
-//   console.log(`Transaction ${id} processed in ${time}ms`);
-// };
-
-// const logError = id => {
-//   console.warn(`Error processing transaction ${id}. Please try again later.`);
-// };
-
-// /*
-//  * Работает так
-//  */
+/*
+ * Работает так
+ */
 // makeTransaction({ id: 70, amount: 150 }, logSuccess, logError);
 // makeTransaction({ id: 71, amount: 230 }, logSuccess, logError);
 // makeTransaction({ id: 72, amount: 75 }, logSuccess, logError);
 // makeTransaction({ id: 73, amount: 100 }, logSuccess, logError);
-// /*
-//  * Должно работать так
-//  */
-// makeTransaction({ id: 70, amount: 150 })
-//   .then(logSuccess)
-//   .catch(logError);
+/*
+ * Должно работать так
+ */
 
-// makeTransaction({ id: 71, amount: 230 })
-//   .then(logSuccess)
-//   .catch(logError);
 
-// makeTransaction({ id: 72, amount: 75 })
-//   .then(logSuccess)
-//   .catch(logError);
+taskMakeTransaction.addEventListener('click', () => {
+    makeTransaction({ id: 70, amount: 150 })
+        .then(logSuccess)
+        .catch(logError);
 
-// makeTransaction({ id: 73, amount: 100 })
-//   .then(logSuccess)
-//   .catch(logError);
+    makeTransaction({ id: 71, amount: 230 })
+        .then(logSuccess)
+        .catch(logError);
+
+    makeTransaction({ id: 72, amount: 75 })
+        .then(logSuccess)
+        .catch(logError);
+
+    makeTransaction({ id: 73, amount: 100 })
+        .then(logSuccess)
+        .catch(logError);
+});
